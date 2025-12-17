c3f6df774a667c87c152f6c288238fbdece177f2
Adetutu <93272708+Adeytee@users.noreply.github.com>
2025-12-17 18:51:22 +0100

    Update vite.config.ts

--- Pacific-Host-Hospitality-Dashboard/vite.config.ts
+++ Pacific-Host-Hospitality-Dashboard/vite.config.ts
@@ -11,24 +11,24 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
           await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
+            m.devBanner()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(import.meta.url, "../client/src"),
+      "@shared": path.resolve(import.meta.url, "../shared"),
+      "@assets": path.resolve(import.meta.url, "../attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(import.meta.url, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.url, "dist"), // <-- change to dist
     emptyOutDir: true,
   },
   server: {
